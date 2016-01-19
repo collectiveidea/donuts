2deaedee7d5294cfb6ea3b5d99abdffafbaebb43
Steve Richert <steve.richert@gmail.com>
2016-01-19 13:22:55 -0500

    Test doubling down

--- spec/features/claims_spec.rb
+++ spec/features/claims_spec.rb
@@ -35,4 +35,39 @@ feature "Claiming a Donut Day" do
     expect(claim.user).to be_present
     expect(claim.date).to be_today
   end
+
+  scenario "A visitor can double down on donuts" do
+    create(:claim, date: Date.current)
+    sign_in_on_github
+
+    visit root_path
+    donuts_button.click
+
+    expect(current_path).to eq(root_path)
+    expect(donuts_button).to be_disabled
+
+    expect(Claim.count).to eq(2)
+
+    claim = Claim.order(:created_at).last
+    expect(claim.user).to be_present
+    expect(claim.date).to be_today
+  end
+
+  scenario "A user can double down on donuts" do
+    create(:claim, date: Date.current)
+    user = create(:user)
+    sign_in_on_github(user)
+
+    visit root_path
+    donuts_button.click
+
+    expect(current_path).to eq(root_path)
+    expect(donuts_button).to be_disabled
+
+    expect(Claim.count).to eq(2)
+
+    claim = Claim.order(:created_at).last
+    expect(claim.user).to be_present
+    expect(claim.date).to be_today
+  end
 end
