ea9cab0c969ba61d6737f6ca96f17d023f35175c
Matt Slack <slack@integernoun.com>
2016-10-07 09:00:59 -0400

    Add meta tags for an unfurl, so things look nice in Slack

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
       link_to user.display_name, user.github_url, target: "_blank"
     }.to_sentence.html_safe
   end
+  def users_list(users)
+    users.map { |user|
+      user.display_name
+    }.to_sentence.html_safe
+  end
 end
